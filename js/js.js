c16a1feee26dcef730d0f1ac9ece1ed670e81863
maxravel <maxmaterniak@gmail.com>
2018-01-10 17:24:08 +0100

    first

---
+++ js/js.js
@@ -0,0 +1,12 @@
+function calc() {
+    const workedHours = parseInt(document.getElementById("hours").value);
+    let overHours;
+    if(workedHours>40){
+        overHours=workedHours-40;
+        document.getElementById("overHours").value=overHours;
+        document.getElementById("earnedMoney").value = 40*30+overHours*40+"$"; 
+    }
+    else{
+        document.getElementById("overHours").value="You don't have over hours.";
+        document.getElementById("earnedMoney").value = workedHours*30+"$";
+}}
---
+++ readme.md
@@ -0,0 +1,2 @@
+simple overtime hours payment calculator  
+to do: addEventListener implementation
